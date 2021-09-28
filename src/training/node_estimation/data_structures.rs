6ac184e58eb7a2cf7cad41bcb9a6d53624700066
wenig <info@pwenig.de>
2021-09-28 16:25:59 +0200

    feat: make IntersectionNode displayable

--- src/training/node_estimation/data_structures.rs
+++ src/training/node_estimation/data_structures.rs
@@ -1,7 +1,14 @@
-
 use crate::training::intersection_calculation::{SegmentID};
+use std::fmt::{Display, Formatter, Result};
+
 
 pub struct IntersectionNode {
     pub segment: SegmentID,
     pub cluster_id: usize
+}
+
+impl Display for IntersectionNode {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{}_{}", self.segment, self.cluster_id)
+    }
 }
